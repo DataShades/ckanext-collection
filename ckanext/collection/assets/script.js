3bb615ac5019219f8072e2f915797f1bd9917b1a
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-10-16 22:57:13 +0300

    chore: init

---
+++ ckanext/collection/assets/script.js
@@ -0,0 +1,10 @@
+ckan.module("collection-module", function ($, _) {
+  "use strict";
+  return {
+    options: {
+      debug: false,
+    },
+
+    initialize: function () {},
+  };
+});
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const { resolve } = require("path");
+const { src, dest, watch } = require("gulp");
+const if_ = require("gulp-if");
+const sourcemaps = require("gulp-sourcemaps");
+const postcss = require("gulp-postcss");
+const sass = require("gulp-sass")(require("sass"));
+const touch = require("gulp-touch-cmd");
+
+const isDev = () => !!process.env.DEBUG;
+
+const assetsDir = resolve(__dirname, "ckanext/collection/assets");
+const srcDir = resolve(assetsDir, "scss");
+const destDir = resolve(assetsDir, "css");
+
+const build = () =>
+  src(resolve(srcDir, "collection.scss"))
+    .pipe(if_(isDev, sourcemaps.init()))
+    .pipe(sass({ outputStyle: "compressed" }).on('error', sass.logError))
+    .pipe(postcss([require("postcss-combine-media-query")]))
+    .pipe(if_(isDev, sourcemaps.write()))
+    .pipe(dest(destDir))
+    .pipe(touch());
+
+const watchStyles = () =>
+  watch(resolve(srcDir, "*.scss"), { ignoreInitial: false }, build);
+
+exports.watch = watchStyles;
+exports.build = build;
