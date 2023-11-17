6ebb0a1e3b445c83dbdc86f6a7a594074e1333e3
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-11-17 16:09:57 +0200

    fix: invariant base collection causes typing error

--- ckanext/collection/types.py
+++ ckanext/collection/types.py
@@ -7,4 +7,4 @@ if TYPE_CHECKING:
     from .utils.collection import BaseCollection
 
 
-TDataCollection = TypeVar("TDataCollection", bound="BaseCollection")
+TDataCollection = TypeVar("TDataCollection", bound="BaseCollection", covariant=True)
