9d5cd80411c3b975561ba1186f2f8250ec5b7d65
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2024-01-30 16:08:32 +0200

    feat: add static_columns to table data

--- ckanext/collection/utils/data/db.py
+++ ckanext/collection/utils/data/db.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import abc
+from typing import Any, Iterable
 
 import sqlalchemy as sa
 from sqlalchemy.sql.selectable import GenerativeSelect, Select
@@ -8,6 +9,9 @@ from sqlalchemy.sql.selectable import GenerativeSelect, Select
 from ckanext.collection import shared, types
 
 from .model import BaseSaData
+
+# from pandas.compat._optional import import_optional_dependency
+# import_optional_dependency("sqlalchemy", errors="ignore")
 
 
 class DbData(BaseSaData[Select, types.TData, types.TDbCollection], abc.ABC):
@@ -17,9 +21,15 @@ class DbData(BaseSaData[Select, types.TData, types.TDbCollection], abc.ABC):
 
 class TableData(DbData[types.TData, types.TDbCollection]):
     table: str = shared.configurable_attribute()
+    static_columns: Iterable[Any] = shared.configurable_attribute(
+        default_factory=lambda self: [],
+    )
 
     def get_base_statement(self):
-        columns = self.attached.db_connection.inspector.get_columns(self.table)
-        return sa.select(*[sa.column(c["name"]) for c in columns]).select_from(
+        columns = self.static_columns or [
+            sa.column(c["name"])
+            for c in self.attached.db_connection.inspector.get_columns(self.table)
+        ]
+        return sa.select(*columns).select_from(
             sa.table(self.table),
         )
